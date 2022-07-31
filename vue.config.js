338a6f0aeee06bb129c62caaa9f0abf4d7767001
ludchieng <lud.chieng@hotmail.fr>
2022-07-31 16:50:24 +0200

    Add workbox injectManifest

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  pwa: {
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      swSrc: './public/sw.js',
+      swDest: 'sw.js',
+    },
+  },
 })
