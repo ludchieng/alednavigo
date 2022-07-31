a76003610e67235cf657d09473509e0584783b44
ludchieng <lud.chieng@hotmail.fr>
2022-07-31 23:50:09 +0200

    [newtab-page] Fix NewTabPage style

--- src/pages/NewTabPage/index.vue
+++ src/pages/NewTabPage/index.vue
@@ -35,15 +35,16 @@ export default Vue.extend({
 }
 
 h2 {
-  font-size: 1rem;
-  margin: 2rem 0 0 0;
+  font-size: 1.1rem;
+  margin: 2rem 0 0.1rem 0;
 }
 
 hr {
   margin: 0;
   height: 2px;
-  width: 13rem;
+  max-width: 15rem;
   background-color: #2f2f2f;
+  border: 0;
 }
 .icon-line {
   margin: 0.5rem 0.5rem 0 0;
