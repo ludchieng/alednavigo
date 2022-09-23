fe05a5e4460bc829c58bf1d0086f3052cd1728cc
ludchieng <lud.chieng@hotmail.fr>
2022-09-23 12:44:58 +0200

    [parser] Add line description tsv parser

---
+++ tests/unit/parser/mockdata/line-1.ts
@@ -0,0 +1,57 @@
+/* eslint-disable quote-props */
+/* eslint-disable no-tabs */
+export default {
+  tsv: `
+  \t@ \tmonitoringRefs \tslugName           \tdisplayName         \tprevStops          \tnextStops         \tlineConnections
+* \t@ \t         58876 \tgournay-sur-chevre \tGournay-sur-Chèvre  \t                   \tclaquos-les-troux \tline-2/gournay-le-vesoul
+| \t@
+* \t@ \t           469 \tclaquos-les-troux  \tClaquos – les Troux \tgournay-sur-chèvre \tcastel-aux-lopez  \t
+* \t@ \t        134008 \tcastel-aux-lopez   \tCastel-aux-Lopez    \tclaquos-les-troux  \t                  \tline-2	
+`,
+  routeMap: [
+    {
+      drawing: ['*'],
+      slugName: 'gournay-sur-chevre',
+      displayName: 'Gournay-sur-Chèvre',
+      lineConnections: ['line-2/gournay-le-vesoul'],
+    },
+    {
+      drawing: ['|'],
+    },
+    {
+      drawing: ['*'],
+      slugName: 'claquos-les-troux',
+      displayName: 'Claquos – les Troux',
+      lineConnections: [],
+    },
+    {
+      drawing: ['*'],
+      slugName: 'castel-aux-lopez',
+      displayName: 'Castel-aux-Lopez',
+      lineConnections: ['line-2/castel-aux-lopez'],
+    },
+  ],
+  stops: {
+    'gournay-sur-chevre': {
+      monitoringRefs: ['58876'],
+      displayName: 'Gournay-sur-Chèvre',
+      prevStops: [],
+      nextStops: ['claquos-les-troux'],
+      lineConnections: ['line-2/gournay-le-vesoul'],
+    },
+    'claquos-les-troux': {
+      monitoringRefs: ['469'],
+      displayName: 'Claquos – les Troux',
+      prevStops: ['gournay-sur-chèvre'],
+      nextStops: ['castel-aux-lopez'],
+      lineConnections: [],
+    },
+    'castel-aux-lopez': {
+      monitoringRefs: ['134008'],
+      displayName: 'Castel-aux-Lopez',
+      prevStops: ['claquos-les-troux'],
+      nextStops: [],
+      lineConnections: ['line-2/castel-aux-lopez'],
+    },
+  },
+}
