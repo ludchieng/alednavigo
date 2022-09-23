0694aeacf544a65b5989af483f7849347fec2adb
ludchieng <lud.chieng@hotmail.fr>
2022-09-23 17:50:10 +0200

    [tab-page-stop] Fix styles

--- src/pages/TabPage/Stop.vue
+++ src/pages/TabPage/Stop.vue
@@ -2,25 +2,29 @@
   <div class="tab-page-stop">
     <h2>
       {{ stop.displayName }}
-      <span class="line-connections">
-        <LineIcon
-          v-for="(conn, i) in stop.lineConnections" :key="i"
-          type="dark" :line="conn.line" />
-      </span>
     </h2>
+    <span class="line-connections">
+      <LineIcon
+        v-for="(conn, i) in stop.lineConnections" :key="i"
+        type="dark" :line="conn.line" />
+    </span>
 
     <div class="prev-next-stops">
       <ul>
-        <li v-for="(prevStop, i) in stop.prevStops" :key="i">
+        <li v-for="(prevStop, i) in stop.prevStops" :key="i" class="prev-stops">
           <router-link :to="`/${$route.params.tab}/${$route.params.line}/${prevStop}`">
-            {{ getStop($route.params.line, prevStop) }}
+            <span>
+              {{ getStop($route.params.line, prevStop) }}
+            </span>
           </router-link>
         </li>
       </ul>
       <ul>
-        <li v-for="(nextStop, i) in stop.nextStops" :key="i">
+        <li v-for="(nextStop, i) in stop.nextStops" :key="i" class="next-stops">
           <router-link :to="`/${$route.params.tab}/${$route.params.line}/${nextStop}`">
-            {{ getStop($route.params.line, nextStop) }}
+            <span>
+              {{ getStop($route.params.line, nextStop) }}
+            </span>
           </router-link>
         </li>
       </ul>
@@ -76,24 +80,28 @@ export default Vue.extend({
 </script>
 
 <style scoped>
+h2 {
+  display: inline-block;
+  margin: 0.5rem 1rem 0 0;
+}
+
 .tab-page-stop {
-  padding: 1rem;
+  padding: 0 1rem;
 }
 
 .line-connections {
-  margin-top: 0.4rem;
   display: inline-block;
 }
 
 .line-connections > img {
   width: 2rem;
-  margin-bottom: 0.4rem;
-  margin-right: 0.4rem;
-  vertical-align: -0.4rem;
+  margin: 0.4rem 0.4rem 0.4rem 0;
+  vertical-align: -0.8rem;
 }
 
 .prev-next-stops {
   display: flex;
+  margin-top: 1rem;
   justify-content: space-between;
   align-items: center;
 }
@@ -102,11 +110,28 @@ export default Vue.extend({
   width: 49%;
 }
 
-.prev-next-stops li {
-  padding: 1rem;
+.prev-next-stops li a {
+  display: flex;
+  padding: 1rem 0;
   margin-bottom: 0.5rem;
   background-color: #dfdfdf;
   border-radius: 0.4rem;
+  text-align: center;
+}
+.prev-next-stops a span {
+  display: inline-block;
+  width: 100%;
+  padding: 0 0.3rem;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+
+.prev-stops a::before {
+  content: '〈';
+}
+.next-stops a::after {
+  content: '〉';
 }
 
 </style>
