9a44c73c5c371e51f88378588f8af2c7f0ff402c
ludchieng <lud.chieng@hotmail.fr>
2022-09-29 12:00:32 +0200

    [refactor-tab-page-stop] Add PrevNextStops

---
+++ src/components/StopTimetable/PrevNextStops.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="prev-next-stops">
+    <ul>
+      <li v-for="(prevStop, i) in stop.prevStops.slice().reverse()" :key="i" class="prev-stops">
+        <router-link :to="`/${$route.params.tab}/${$route.params.line}/${prevStop}`">
+          <span>
+            {{ getStop($route.params.line, prevStop).displayName }}
+          </span>
+        </router-link>
+      </li>
+    </ul>
+    <ul>
+      <li v-for="(nextStop, i) in stop.nextStops.slice().reverse()" :key="i" class="next-stops">
+        <router-link :to="`/${$route.params.tab}/${$route.params.line}/${nextStop}`">
+          <span>
+            {{ getStop($route.params.line, nextStop).displayName }}
+          </span>
+        </router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from 'vue'
+import { StopType } from '@/utils/parser'
+
+export default Vue.extend({
+  name: 'StopTimetablePrevNextStops',
+  props: {
+    stop: {} as PropType<StopType>,
+  },
+  methods: {
+    getStop (line: string, slugName: string) {
+      return JSON.parse(
+        localStorage.getItem(`lines.${line}.stops`) as string,
+      )[slugName]
+    },
+  },
+})
+</script>
+
+<style scoped>
+.prev-next-stops {
+  display: flex;
+  margin-top: 1rem;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.prev-next-stops ul {
+  width: 49%;
+}
+
+.prev-next-stops li a {
+  display: flex;
+  padding: 0.8rem 0;
+  margin-bottom: 0.5rem;
+  font-size: 0.95rem;
+  background-color: #dfdfdf;
+  border-radius: 0.4rem;
+  text-align: center;
+}
+.prev-next-stops a span {
+  display: inline-block;
+  width: 100%;
+  padding: 0.1rem 0.3rem 0 0.3rem;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+
+.prev-stops a::before {
+  content: '〈';
+}
+.next-stops a::after {
+  content: '〉';
+}
+</style>
--- src/components/StopTimetable/index.vue
+++ src/components/StopTimetable/index.vue
@@ -1,27 +1,7 @@
 <template>
   <div>
     <StopTimetableHeader :stop="stop" />
-
-    <div class="prev-next-stops">
-      <ul>
-        <li v-for="(prevStop, i) in stop.prevStops.slice().reverse()" :key="i" class="prev-stops">
-          <router-link :to="`/${$route.params.tab}/${$route.params.line}/${prevStop}`">
-            <span>
-              {{ getStop($route.params.line, prevStop).displayName }}
-            </span>
-          </router-link>
-        </li>
-      </ul>
-      <ul>
-        <li v-for="(nextStop, i) in stop.nextStops.slice().reverse()" :key="i" class="next-stops">
-          <router-link :to="`/${$route.params.tab}/${$route.params.line}/${nextStop}`">
-            <span>
-              {{ getStop($route.params.line, nextStop).displayName }}
-            </span>
-          </router-link>
-        </li>
-      </ul>
-    </div>
+    <PrevNextStops :stop="stop" />
 
     <pre>{{ Array.from(debugData).reduce((acc, line) => acc + `\n${line}`, '') }}</pre>
 
@@ -62,11 +42,13 @@ import Vue from 'vue'
 import { StopType } from '@/utils/parser'
 import { VisitType } from '@/utils/fetcher'
 import StopTimetableHeader from '@/components/StopTimetable/Header.vue'
+import PrevNextStops from '@/components/StopTimetable/PrevNextStops.vue'
 
 export default Vue.extend({
   name: 'TabPageStop',
   components: {
     StopTimetableHeader,
+    PrevNextStops,
   },
   props: {
   },
@@ -170,43 +152,6 @@ export default Vue.extend({
 </script>
 
 <style scoped>
-
-.prev-next-stops {
-  display: flex;
-  margin-top: 1rem;
-  justify-content: space-between;
-  align-items: center;
-}
-
-.prev-next-stops ul {
-  width: 49%;
-}
-
-.prev-next-stops li a {
-  display: flex;
-  padding: 0.8rem 0;
-  margin-bottom: 0.5rem;
-  font-size: 0.95rem;
-  background-color: #dfdfdf;
-  border-radius: 0.4rem;
-  text-align: center;
-}
-.prev-next-stops a span {
-  display: inline-block;
-  width: 100%;
-  padding: 0.1rem 0.3rem 0 0.3rem;
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-}
-
-.prev-stops a::before {
-  content: '〈';
-}
-.next-stops a::after {
-  content: '〉';
-}
-
 .timetables {
   margin-top: 1.5rem;
 }
