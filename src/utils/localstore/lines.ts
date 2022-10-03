61b7e4b5e7aaa97ecf5360dc54cdbed5c47dc08a
ludchieng <lud.chieng@hotmail.fr>
2022-10-03 12:12:21 +0200

    [refactor-localstore] Refactor some getters

---
+++ src/utils/localstore/lines.ts
@@ -0,0 +1,30 @@
+import { LineDescriptionType } from '../parser'
+
+export const setLine = (line: string, lineDescription: Partial<LineDescriptionType>) => {
+  Object.entries(lineDescription).forEach(([key, value]) => {
+    localStorage.setItem(`lines.${line}.${key}`, JSON.stringify(value))
+  })
+}
+
+export const getLinesByRef = () => {
+  return JSON.parse(localStorage.getItem('lines') as string)
+}
+
+export const getLinesByCategory = () => {
+  const res = {} as { [x: string]: any }
+  const lines = Object.entries(getLinesByRef())
+    .filter(([k, v]) => k.trim() !== '')
+    .map(([k, v]) => v) as any[]
+
+  for (const line of lines) {
+    if (!res[line.category]) res[line.category] = []
+    res[line.category].push({ ...(delete line.category, line) })
+  }
+
+  return Object.entries(res)
+    .map(([category, lines]) => ({
+      name: category,
+      lines,
+    }))
+    .filter((category) => !category.name.includes('TER'))
+}
