5b9522f8c3d521ea06412ea368e23d42aa26a10c
ludchieng <lud.chieng@hotmail.fr>
2023-01-24 23:17:19 +0100

    refactor: add DesignTitle

--- src/components/Timetables/TimetablesHome.vue
+++ src/components/Timetables/TimetablesHome.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="tab-page-new">
-    <div v-for="category in linesByCategory" :key="category.name">
-      <h2>{{ category.name }}</h2>
-      <hr />
-      <router-link v-for="line in category.lines" :key="line.slugName"
-        class="icon-line" :to="`/timetables/${$route.params.tab}/${line.slugName}`"
+    <div
+      v-for="category in linesByCategory"
+      :key="category.name"
+    >
+      <DesignTitle>{{ category.name }}</DesignTitle>
+      <router-link
+        v-for="line in category.lines"
+        :key="line.slugName"
+        class="icon-line"
+        :to="`/timetables/${$route.params.tab}/${line.slugName}`"
       >
         <LineIcon
-          :lineSlugName="line.slugName"
+          :line-slug-name="line.slugName"
           color="dark"
           size="lg"
-          fadeIn
+          fade-in
         />
       </router-link>
     </div>
@@ -21,10 +26,14 @@
 import Vue from 'vue'
 import LineIcon from '@/components/LineIcon.vue'
 import { getLinesByCategory } from '@/utils/localstore/lines'
+import DesignTitle from '@/components/design/DesignTitle.vue'
 
 export default Vue.extend({
   name: 'TimetablesHome',
-  components: { LineIcon },
+  components: {
+    LineIcon,
+    DesignTitle,
+  },
   computed: {
     linesByCategory () {
       return getLinesByCategory()
@@ -36,19 +45,6 @@ export default Vue.extend({
 <style scoped>
 .tab-page-new {
   padding: 1rem;
-}
-
-h2 {
-  font-size: 1.1rem;
-  margin: 2rem 0 0.1rem 0;
-}
-
-hr {
-  margin: 0;
-  height: 2px;
-  max-width: 15rem;
-  background-color: #2f2f2f;
-  border: 0;
 }
 
 .icon-line {
