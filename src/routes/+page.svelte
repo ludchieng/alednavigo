0b265322ce08c6f117579fc2d2c8aa7f3c5d1972
Ludwig Chieng <lud.chieng@hotmail.fr>
2023-08-31 22:09:14 +0200

    feat: add basic routes

--- src/routes/+page.svelte
+++
@@ -1,7 +0,0 @@
-<svelte:head>
-  <title>Hello world!</title>
-  <meta name="description" content="Consultez les horaires temps-réel fournis par Île-de-France Mobilités pour les services voyageurs SNCF et RATP. Application non-officielle développée par un tier bénévole." />
-</svelte:head>
-
-<h1>Alednavigo</h1>
-<a href="/app/metro-7/opera">(7) Opéra</a>
---
+++ src/routes/timetables/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+</script>
+
+<h1>Alednavigo</h1>
+<a href="/timetables/metro-7/opera">(7) Opéra</a>
