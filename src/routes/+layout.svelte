f764b50166b3e7c209b6ab3ad1df4b38e1f033b2
ludchieng <lud.chieng@hotmail.fr>
2023-12-23 13:23:58 +0100

    chore: update packages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,39 +1,35 @@
 <script>
-	import Layout from "$lib/ui/Layout.svelte";
-	import Header from "$lib/ui/Header.svelte";
-	import breadcrumbBracket from '$lib/ui/img/breadcrumb-bracket.svg'
+	import Layout from '$lib/ui/Layout.svelte';
+	import Header from '$lib/ui/Header.svelte';
+	import '$lib/ui/css/main.css';
+	import breadcrumbBracket from '$lib/ui/img/breadcrumb-bracket.svg';
 </script>
 
 <Layout>
 	<Header slot="header">
 		<div slot="left" class="header-left">
 			<a href="/">
-				<img class="logo" src="/img/app-icons/icon.png" alt="Accueil">
+				<img class="logo" src="/img/app-icons/icon.png" alt="Accueil" />
 			</a>
-			<img src={breadcrumbBracket} alt="">
+			<img src={breadcrumbBracket} alt="" />
 			<a href={`/timetables/rer-a/`}>
-				<img class="line" src="/img/lines-icons/colors/rer-a.svg" alt="Ligne RER A">
+				<img class="line" src="/img/lines-icons/colors/rer-a.svg" alt="Ligne RER A" />
 			</a>
 		</div>
 
-		<div slot="center">
-		</div>
+		<div slot="center"></div>
 
-		<div slot="right">
-			00:00:00
-		</div>
+		<div slot="right">00:00:00</div>
 	</Header>
 
 	<slot slot="main" />
-	
+
 	<div slot="footer">
 		<p>Footer</p>
 	</div>
 </Layout>
 
 <style>
-	@import '$lib/ui/css/main.css';
-
 	.line,
 	.logo {
 		display: block;
