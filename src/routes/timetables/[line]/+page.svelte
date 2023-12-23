574b44c03f044ad880a805101ca6f0c09232865a
Ludwig Chieng <ludwig.chieng@octo.com>
2023-12-23 14:26:45 +0100

    fix: misc

--- src/routes/timetables/[line]/+page.svelte
+++ src/routes/timetables/[line]/+page.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
-	import { page } from "$app/stores";
+  import { page } from '$app/stores';
+  import type { PageData } from './$types';
+
+  export let data: PageData;
 </script>
 
 <p>{$page.params.line}</p>
